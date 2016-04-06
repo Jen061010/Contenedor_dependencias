8d5870ec40816e90de69f72f7866e8be4b56e8a0
Jennifer <jvs1192@gmail.com>
2016-04-06 09:43:00 +0200

    Add comments

--- Contenedor de dependencias/Program.cs
+++ Contenedor de dependencias/Program.cs
@@ -13,8 +13,11 @@ namespace Contenedor_dependencias
             //var service=new Service(new Repository);
             /*IService service= new Service(new Repository());
             service.Add();*/
+
+            //Ahora vamos a hacer lo mismo que anteriormente pero por reflexion
             var serviceType = Type.GetType("Contenedor_dependencias.Service");
             var repositoryType = Type.GetType("Contenedor_dependencias.Repository");
+            //Sólo tenemos un constructor pero no se usa getConstructor porque se tendrían que indicar los tipos
             var IRepository=repositoryType.GetConstructors()[0].Invoke(new object[]{});
             var IService=serviceType.GetConstructors()[0].Invoke(new object[] { IRepository });// Estoy almacenando la clase repository
             serviceType.GetMethod("Add").Invoke(IService,new object[]{});
